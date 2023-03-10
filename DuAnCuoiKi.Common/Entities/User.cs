7069dd1a66a47caac19535a7e386f4d2c71853f5
HuyTo1368 <huyto1368@gmail.com>
2023-03-10 09:18:26 +0700

    Add project files.

---
+++ DuAnCuoiKi.Common/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuAnCuoiKi.Common.Entities
+{
+    /// <summary>
+    /// Thông tin đăng nhập
+    /// </summary>
+    internal class User
+    {
+        public Guid UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
---
+++ DuAnCuoiKi.DL/AuthDL/LoginDL.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+using DuAnCuoiKi.Common.Entities
+
+namespace DuAnCuoiKi.DL.AuthDL
+{
+    public class LoginDL:ILoginDL
+    {
+        public object CheckLogin(string userName, string UserPassword) {
+            object user;
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add($"userName", userName);
+            parameters.Add($"userPassword", UserPassword);
+            string storeProcedureName = string.Format(Procedures.Proc_InforOne, typeof(User).Name);
+            return { }
+        };
+    }
+}
